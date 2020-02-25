2abe43c9d32951b3449ab153ed33d51826928d21
Bobby <bobbybuntin@learnsfdc.com>
2020-02-25 11:26:46 -0500

    Working prototype with columns and object set by app builder attributes

--- force-app/main/default/lwc/relatedListLWCcomponent/relatedListLWCcomponent.js
+++ force-app/main/default/lwc/relatedListLWCcomponent/relatedListLWCcomponent.js
@@ -2,10 +2,12 @@ import { LightningElement, track, wire, api } from 'lwc';
 //import fetchDataHelper from './fetchDataHelper';
 //import findContacts from '@salesforce/apex/accountRelatedContacts.getContacts';
 import getRelatedRecords from '@salesforce/apex/comboRelatedListController.getRelatedRecords';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+//import getColumns from '@salesforce/apex/comboRelatedListController.getColumns';
 //import getIconName from '@salesforce/apex/accountRelatedContacts.getContacts';
  
 // Datatable Columns
-const columns = [
+/*const columns = [
     {
         label: 'Name',
         fieldName: 'UrlName',
@@ -24,7 +26,8 @@ const columns = [
         type: 'text'
     }
 ];
-
+*/
+var columns = [];
 
 
 export default class RelatedListLWCcomponent extends LightningElement {
@@ -38,34 +41,72 @@ export default class RelatedListLWCcomponent extends LightningElement {
     @api columnFields;
     @api urlField;
 
+    @wire(getObjectInfo, { objectApiName: '$chosenObject' })
+    objectInfo;
+
     //@wire(getIconName, { sObjectName : '$relatedObjectName'}) iconName;
 
     @wire(getRelatedRecords, { searchKey: '$searchKey', objectName: '$objectApiName', relatedObjectName: '$relatedObjectName', columnFields: '$columnFields', urlField: '$urlField'})
     relatedRecords;
-    /*
-    contacts(result)
-    {
-        const { data, error } = result;
-        if(data) {
-            let nameUrl;
-            this.contacts = data.map(row => { 
-                nameUrl = `/${row.Id}`;
-                return {...row , nameUrl} 
-            })
-            this.error = null;
+
+    handleColumns() {
+        var columnArray = this.columnFields.split(',').map(item => item.trim());
+        var urlColumnsArray = this.urlField.split(',').map(item => item.trim());
+
+        var columns2 = [];
+
+        if(urlColumnsArray.length > 0)
+        {
+            for (var x in urlColumnsArray)
+            {
+                var theColumnDefinition = {
+                    label: urlColumnsArray[x],
+                    fieldName: 'UrlName',
+                    type: 'url',
+
+                    typeAttributes: {label: {fieldName: urlColumnsArray[x]},
+                    target: '_self'},
+                    sortable: true
+                };
+                columns2.push(theColumnDefinition);
+            }
         }
-        if(error) {
-            this.error = error;
-            this.contacts = [];
+
+        if(columnArray.length > 0)
+        {
+            for (var x in columnArray)
+            {
+                var theColumnDefinition = {
+                    label : columnArray[x],
+                    fieldName : columnArray[x],
+                    type : 'text'
+                };
+                columns2.push(theColumnDefinition);
+            }
         }
+        console.log(columns2);
+        console.log(this.relatedRecords);
+        this.columns = columns2;
     }
-    */
+
 
     connectedCallback() {
         this.searchKey = this.recordId;
         this.relatedObjectName = this.chosenObject;
-        //this.chosenObject = this.recordId;
+        this.handleColumns();
     }
+
+    //TODO
+    //Add handling of count of records / pagination
+    //Fix Icon and header formatting
+    //Make the field type match the actual datatype instead of always String
+    //Create test classes
+    //Add error handling
+    //Add attribute checkbox to optionally include relationship name as a column
+    //Replace column labels with the field label instead of the api name
+    //Add handling to ensure a reasonable number of max columns
+    //enbale column sorting options
+
 
     
 }
